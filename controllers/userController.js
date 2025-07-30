69478be7fc4285468c01ee7c30d29f7b092534d9
Cristian <medinacristian282@gmail.com>
2025-07-30 12:07:26 -0700

    secure cors

--- controllers/userController.js
+++ controllers/userController.js
@@ -51,7 +51,7 @@ const loginUser = async (req, res) => {
     res
       .cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', 
+        secure: true, 
         sameSite: 'None', 
         maxAge: 60 * 60 * 1000, 
       })
